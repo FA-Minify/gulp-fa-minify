3dd71861285995a82c4897d315fdf4eaf2b9d831
Markus Seiffert <m.seiffert@snapaddy.com>
2018-07-20 21:41:34 +0200

    feat: extract core function in own repo (fa-minify)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const faMinify = require('fa-minify');
 const through = require('through2');
 
 const PLUGIN_NAME = 'gulp-fa-minify';
@@ -20,59 +21,13 @@ module.exports = function (usedIcons) {
 
     if (file.isBuffer()) {
 
-      let content = file.contents.toString();
-
-      // fontawesoms all.js consists of n+1 functions where n=number of different icon styles (far,fas,fal,fab)
-      content = content.replace(/(\(function\s*\(\)\s*\{[\s\S.]*?}\(\)\);)/gim, function () {
-        const func = arguments[1];
-
-        // get the type ('far'/'fas'/'fal'/'fab') from function
-        const matches = /\('(fa.)',/gi.exec(arguments[1]) || [];
-        const type = matches[1];
-
-        // no type means this function is the bootstrap function
-        // keep it
-        if (!type) {
-          return func;
-        }
-
-        if (usedIcons[type] && usedIcons[type].length > 0) {
-          // we use some icons from this type so we keep this function
-          return func;
-        }
-
-        // no icons used from this type so remove the function
-        return '';
-      });
-
-      // we search for the icons object, parse it and remove unused icons
-      content = content.replace(/(var\s+icons\s*=)([\s\S.]*?)(;[\s\S.]*?define\('(fa.)', icons\);)/gmi, function () {
-        const icons = arguments[2];
-        const type = arguments[4];
-
-        // parse the icons object read from the file content
-        let iconObject = null;
-        try {
-          iconObject = JSON.parse(icons)
-        } catch (e) {
-          iconObject = {};
-        }
-        
-        // keep usedIcons and remove every other icon
-        if (usedIcons[type]) {
-          Object.keys(iconObject).forEach(key => {
-            if (!usedIcons[type].includes(key)) {
-              delete iconObject[key];
-            }
-          });
-        }
-
-        // return the code without unused icons
-        return arguments[1] + JSON.stringify(iconObject) + arguments[3];
-      });
+      const content = file.contents.toString();
+      const minified = faMinify(content, { usedIcons });
 
       // set the fileContent
-      file.contents = Buffer.from(content);
+      if (minified) {
+        file.contents = Buffer.from(minified);
+      }
     }
 
     // make sure the file goes through the next gulp plugin
