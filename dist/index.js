7b233464a1454add27853999f8570dcddcb5b33b
NetWin <netwin@codedcomplexity.de>
2022-02-13 05:47:36 +0100

    chore: remove dist folder

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const plugin_1 = require("./plugin");
-module.exports = plugin_1.plugin;
--- dist/plugin.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-import { Transform } from 'stream';
-import { IconType } from 'fa-minify';
-export declare function plugin(usedIcons: {
-    [type in IconType]?: string[];
-}): Transform;
